727bd7d564753bec3839c5b9a51a7e5b22e53d7e
Julia Hytovska <mejulia716@gmail.com>
2023-01-25 14:40:53 +1300

    salary homework

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstMonth=+prompt("enter your first month salary");
+let secondMonth=+prompt("enter your second month salary");
+
+let totalSalary=firstMonth+secondMonth
+alert(`Your salary for two moth is ${++totalSalary}`);
+alert(`You have extra bonus! Your total salary was ${totalSalary}}. It increase for 1$! And now it is ${++totalSalary}`);
+
+if(++totalSalary<2000){alert(`im out`)}
+else{alert(`im ready to work`)}
